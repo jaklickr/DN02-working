737225792622de2b3292da3afdf3c82c42a50cc2
akcabra <ak1790@student.uni-lj.si>
2022-04-17 23:35:38 +0200

    Implementiranje grafa, filtrov in slikovne predstavitve jezikov

--- public/skripte/seznam.js
+++ public/skripte/seznam.js
@@ -72,16 +72,126 @@ $(document).ready(() => {
     sifranti = podatki.sifranti;
     filmi = podatki.filmi;
     let parametri = ["leto", "zanr"];
-
+    
     parametri.forEach((parameter) => {
+      //console.log(sifranti["leto"]);
       $("#" + parameter + "-stevilo").html(sifranti[parameter].length);
-      $("#" + parameter + "-izbira").append("<option val=''>...</option>");
+      $("#" + parameter + "-izbira").append("<option value=''>...</option>");
       sifranti[parameter].forEach((vrednost) => {
         $("#" + parameter + "-izbira").append(
           "<option value='" + vrednost + "'>" + vrednost + "</option>"
         );
       });
     });
+    
+    function filterPoFilmih (leto, zanr) {
+      var filtriraniFilmi = [];
+
+      filmi.forEach(film => {
+        if (leto == "" && zanr == "") {
+          filtriraniFilmi.push(film);
+        } else if (leto == "" && zanr != "") {
+          if(film.zanri.split(",").includes(zanr)) {
+            filtriraniFilmi.push(film);
+          }
+        } else if (zanr == "" && leto != "") {
+          if(film.datumIzdaje.split("-", 1)[0] == leto) {
+            filtriraniFilmi.push(film);
+          }
+        } else {
+          if(film.zanri.split(",").includes(zanr) && film.datumIzdaje.split("-", 1)[0] == leto) {
+            filtriraniFilmi.push(film);
+          }
+        }
+      });
+
+      return filtriraniFilmi;
+    }
+
+    var letoFilter = document.getElementById("leto-izbira");
+    var zanrFilter = document.getElementById("zanr-izbira");
+    var seznamFilmov = document.getElementById("filmi");
+
+    function letaPoFiltru(filmi) {
+      var leta = [];
+
+      filmi.forEach(film => {
+        if(!leta.includes(film.datumIzdaje.split("-", 1)[0])) {
+          leta.push(film.datumIzdaje.split("-", 1)[0]);
+        }
+      });
+
+      return leta;
+    }
+
+    function zanriPoFiltru(filmi) {
+      var zanri = [];
+
+      filmi.forEach(film => {
+        for (var i = 0; i < film.zanri.split(",").length; i++) {
+          if (!zanri.includes(film.zanri.split(",")[i])) {
+            zanri.push(film.zanri.split(",")[i]);
+          }
+        }
+      })
+
+      return zanri;
+    }
+
+    function idjiPoFiltru(filmi) {
+      var idji = [];
+
+      filmi.forEach(film => {
+        if (!idji.includes(film.id)) {
+          idji.push(film.id.toString());
+        }
+      })
+
+      return idji;
+    }
+
+    function posodobiFiltre(filmi) {
+      $("#zanr-stevilo").html(zanriPoFiltru(filmi).length);
+      $("#leto-stevilo").html(letaPoFiltru(filmi).length);
+
+      for (var i = 1; i <= sifranti["leto"].length; i++) {
+        if (letaPoFiltru(filmi).includes(letoFilter[i].value)) {
+          letoFilter[i].disabled = false;
+        } else {
+          letoFilter[i].disabled = true;
+        }
+      }
+      for (var i = 1; i <= sifranti["zanr"].length; i++) {
+        if (zanriPoFiltru(filmi).includes(zanrFilter[i].value)) {
+          zanrFilter[i].disabled = false;
+        } else {
+          zanrFilter[i].disabled = true;
+        }
+      }
+    }
+
+    function filtrirajFilme(filmi) {
+      var izbire = document.querySelectorAll(".film");
+      izbire.forEach(film => {
+        if (idjiPoFiltru(filmi).includes(film.id)) {
+          film.getElementsByTagName("button")[0].style.opacity = "1.0";
+        } else {
+          film.getElementsByTagName("button")[0].style.opacity = "0.3";
+        }
+      });
+    }
+
+    letoFilter.addEventListener("change", () => {
+      posodobiFiltre(filterPoFilmih(letoFilter.value, zanrFilter.value));
+      filtrirajFilme(filterPoFilmih(letoFilter.value, zanrFilter.value));
+    })
+    
+    zanrFilter.addEventListener("change", () => {
+      posodobiFiltre(filterPoFilmih(letoFilter.value, zanrFilter.value));
+      filtrirajFilme(filterPoFilmih(letoFilter.value, zanrFilter.value));
+
+    })
+    
   });
 
   // Posodobi podatke iz košarice na spletni strani
@@ -115,4 +225,132 @@ $(document).ready(() => {
   // Klik na gumba za pripravo računov
   $("#racun_html").click(() => (window.location = "/izpisiRacun/html"));
   $("#racun_xml").click(() => (window.location = "/izpisiRacun/xml"));
+
+  
+
+  // Graf
+  window.onload = function() {
+    var CFR = [];
+    var DTA = [];
+    var CFRpoLetih = {};
+    var DTApoLetih = {};
+    
+    $.get("/filtri", (podatki) => {
+    filmi = podatki.filmi;
+
+    function stCFR() {
+      var stCFRI = 0;
+      filmi.forEach((film) => {
+        if(film.zanri.includes("Comedy") || film.zanri.includes("Family") || film.zanri.includes("Romance")) {
+          stCFRI++;
+        }
+      });
+      return stCFRI;
+    }
+
+    function stDTA() {
+      var stDTAI = 0;
+      filmi.forEach((film) => {
+        if (film.zanri.includes("Drama") || film.zanri.includes("Thriller") || film.zanri.includes("Action")) {
+          stDTAI++;
+        }
+      });
+      return stDTAI;
+    }
+    
+    function parseDataPoints () {
+      filmi.forEach((film) => {
+        if(film.zanri.includes("Comedy") || film.zanri.includes("Family") || film.zanri.includes("Romance")) {
+          var leto = zaokrozi(film.datumIzdaje.split("-", 1)[0]);
+          if (leto in CFRpoLetih) {
+            CFRpoLetih[leto] += 1;
+          } else {
+            CFRpoLetih[leto] = 1;
+          }
+        } 
+        if (film.zanri.includes("Drama") || film.zanri.includes("Thriller") || film.zanri.includes("Action")) {
+          leto = zaokrozi(film.datumIzdaje.split("-", 1)[0]);
+          if (leto in DTApoLetih) {
+            DTApoLetih[leto] += 1;
+          } else {
+            DTApoLetih[leto] = 1;
+          }
+        }
+      });
+      Object.keys(CFRpoLetih).forEach(leto => {
+        CFR.push({
+          x: parseInt(leto),
+          y: CFRpoLetih[leto]});
+      });
+      Object.keys(DTApoLetih).forEach(leto => {
+        DTA.push({
+          x: parseInt(leto),
+          y: DTApoLetih[leto]});
+      });
+    }
+      
+      var chart = new CanvasJS.Chart("chartContainer", {
+        title:{
+          text: "Najboljši filmi čez čas",
+          fontColor: "#580000"
+        },
+        subtitles:[{
+          text: "grupirani podatki",
+          fontColor: "#009900"
+        }
+        ],
+        axisY:{
+          title : "Število filmov",
+          crosshair: {
+            snapToDataPoint: true,
+            enabled: true,
+          }
+        },
+        axisX:{
+          valueFormatString: "####",
+          crosshair: {
+            valueFormatString: "####",
+            snapToDataPoint: true,
+            enabled: true,
+          }
+        },
+        
+        data: [
+        {
+          type: "line",
+          lineDashType: "dash",
+          showInLegend: true,
+          legendText: "Komedije, družinski in romance (" + stCFR() + ")",
+          xValueFormatString: "####",
+          dataPoints : CFR,
+          markerType: "circle",
+          color: "#FFA500",
+          //markerColor: "#FFA500"
+        },
+        {
+          type: "line",
+          showInLegend: true,
+          legendText: "Drame, Akcije, Trilerji ("  + stDTA() + ")",
+          xValueFormatString: "####",
+          dataPoints : DTA,
+          markerType: "square",
+          color: "#00468b",
+          //markerColor: "#00468b"
+        },
+        ]
+      });
+      //console.log(DTA);
+      //console.log(CFR);
+      parseDataPoints();
+      chart.render();
+    });
+  };
+
+  function zaokrozi(num) {
+    if (num % 10 < 5) {
+      return Math.floor(num / 10) * 10;
+    } else {
+      return (Math.floor(num / 10) * 10) + 5;
+    }
+  };
 });
